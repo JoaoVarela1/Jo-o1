fdba75bf7c460b35c64f8493a394895b12660179
JoaoVarela1 <84931110+JoaoVarela1@users.noreply.github.com>
2021-06-08 17:16:02 -0300

    Add files via upload

---
+++ PythonApplication5.py
@@ -0,0 +1,40 @@
+Nome="João"
+print(Nome)
+
+age=11
+print(age)
+
+Palhaço=48*54
+Numero=101-67
+Numero1=1002+108
+Numero2=1+2+3+4+5+6+7+8+9+10
+print(Palhaço)
+print(Numero)
+print(Numero1)
+print(Numero2)
+
+age=2020-2009
+print(age)
+
+Ambiente="O Dia Esta Muito Bonito"
+print(Ambiente+" "+Ambiente+"!"+Ambiente)
+
+Comida="batata"
+print(Comida.upper())
+
+Comida="BATATA"
+print(Comida.lower())
+
+age=age+1
+age=age*2
+print(age)
+print("Bom Dia")
+Dia="Bom\nDia"
+print(Dia)
+
+Ultima="\\novo"
+print(Ultima)
+
+
+
+
